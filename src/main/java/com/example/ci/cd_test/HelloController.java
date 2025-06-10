1c4bb10140fad02360f2d116106d4e47122f6dce
yoonsic2 <mrk1251@naver.com>
2025-06-10 15:57:30 +0900

    Initial commit

---
+++ src/main/java/com/example/ci/cd_test/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.ci.cd_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello CI/CD!";
+    }
+}
---
+++ src/test/java/com/example/ci/cd_test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ci.cd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
